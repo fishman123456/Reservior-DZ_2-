7c0719351c7bce867b45dc7db85a834b19ade4a5
F_D <den9152@yandex.ru>
2022-06-22 21:07:04 +0700

    3
    рализовал два метода, поправил конструктор по умолчанию

--- Reservior(DZ_2).cpp
+++ Reservior(DZ_2).cpp
@@ -19,34 +19,55 @@
 using namespace std;
 class Reservior {
 private:
-	string name;
+	
 	double length;
 	double width;
 	double height;
-
+	string name;
+	double squ;
 public:
 	Reservior(double Plength, double Pwidth, double Pheight, string Pname)
 		:length{ Plength }, width{ Pwidth }, height{ Pheight }, name{ Pname }
 	{
 		cout << "\nработает конструктор с параметрами\n";
 	}
-	Reservior() {}
-	~Reservior(){}
+	Reservior(): Reservior(0,0,0,"0") {}
+	~Reservior() {
+		cout << "\n Работает деструктор\n";
+	}
+	// volume
+	//  1 Определения приблизительного обьема (ширина, длина, максимальная глубина)
+	double volume(double Plength, double Pwidth, double Pheight, string Pname) 
+	{ 
+		double vol =
+			(Plength * Pwidth * Pheight);
+		cout << endl << "Обьем водоема = " << vol << " m3" << endl;
+		return vol;
 
+	}
+	// 2 Определение площади водной поверхности
+	double square (double Plength, double Pwidth,  string Pname)
+	{
+
+		double squ =
+			(Plength * Pwidth);
+		cout << endl << "Площадь водоема = " << squ << " m3" << endl;
+		return squ;
+
+	}
+	void print()
+	{
+		
+	}
 };
 
 int main()
 {
 	system("chcp 1251");
+	Reservior one;
+	one.volume(3.2, 6.4, 5.2, "ванная");
+	one.square(2, 2, "бассейн");
+	Reservior two;
+	
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
